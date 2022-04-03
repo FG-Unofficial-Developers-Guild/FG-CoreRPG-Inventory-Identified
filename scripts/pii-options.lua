caeb3c07b00221dcf0de7ec0aaf2c455f2903549
Wil Thieme <wil.thieme@protonmail.com>
2022-04-03 08:44:03 -0400

    line break

--- scripts/pii-options.lua
+++ scripts/pii-options.lua
@@ -1,6 +1,7 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
+
 --	search
 function onInit()
 	OptionsManager.registerOption2(
