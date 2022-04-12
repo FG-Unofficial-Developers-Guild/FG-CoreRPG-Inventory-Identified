af87d9786cce7771fcca0d5f61256aecb4ea9814
Wil Thieme <wil.thieme@protonmail.com>
2022-04-11 21:46:23 -0400

    lua formatting changes and luacheck global defs

--- scripts/pii-options.lua
+++ scripts/pii-options.lua
@@ -1,7 +1,6 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-
 --	search
 function onInit()
 	OptionsManager.registerOption2(
